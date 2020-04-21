2c84e8bc2033e0891938379c75340b442d332385
Callum <callumgolds@hotmail.com>
2020-04-21 13:32:59 -0700

    Windows compat changes WIP

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
-#!/bin/bash -e
+# la de dah.  !/bin/bash -e
 
 if ! type u3d > /dev/null 2>&1; then
-  sudo gem install u3d
+  gem install u3d
   export LC_ALL=en_US.UTF-8
   export LANG=en_US.UTF-8
-  [ "$(uname -s | cut -c -5)" = 'Linux' ] && sudo u3d dependencies
+  [ "$(uname -s | cut -c -5)" = 'Linux' ] &&  u3d dependencies
 fi
 
-sudo u3d install $UNITY_VERSION -p Unity,${BUILD_TARGET}
+#. no no.   u3d install $UNITY_VERSION -p Unity,${BUILD_TARGET}
+u3d install $UNITY_VERSION -p Unity
 
 script_path=$(cd $(dirname $0); pwd)
-rsync -a ${script_path}/Assets/ ${PROJECT_PATH}/Assets/
+cp -R ${script_path}/Assets ${PROJECT_PATH}/Assets/
 
 set +e
 u3d -u $UNITY_VERSION -- -projectPath $PROJECT_PATH -batchmode -nographics -quit -silent-crashes -logFile editor.log -username $UNITY_USERNAME -password $UNITY_PASSWORD -serial $UNITY_SERIAL -buildTarget $BUILD_TARGET -executeMethod $EXECUTE_METHOD -outputPath $OUTPUT_PATH $COMMAND_ARGS
 exit_code=$?
-cat editor.log
+type editor.log
 exit $exit_code
